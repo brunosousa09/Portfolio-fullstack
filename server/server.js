0dad2afacef20433a92d513519f8b382ecef5168
brunosousa09 <bruno-boy-sousa@hotmail.com>
2025-07-05 13:28:26 -0300

    Prepara servidor para o deploy no Render

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.send('Hello from the server teste !');
